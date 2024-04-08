ed996af725931e00d2161da3e8e8c5e60a68075f
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2024-04-08 15:56:18 +0300

    chore: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  srcDir: 'src/',
+  devtools: {
+    enabled: false,
+  },
+  typescript: {
+    strict: true,
+    typeCheck: true,
+  },
+})
