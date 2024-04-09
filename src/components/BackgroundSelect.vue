e4f54d0b325a0ffb47061b540de9a6d3782638e2
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2024-04-09 20:46:32 +0300

    feat: add the background select

---
+++ src/components/BackgroundSelect.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+interface Props {
+  options: SelectOption[]
+}
+
+const { options } = defineProps<Props>()
+
+const background = defineModel<SelectOption>({ required: true })
+</script>
+
+<template>
+  <USelectMenu
+    v-model="background"
+    :options="options"
+    option-attribute="name"
+    class="w-full"
+  >
+    <template #label>
+      <div
+        class="rounded-full w-5 h-5"
+        :style="{ background: background.value }"
+      />
+    </template>
+
+    <template #option="{ option }">
+      <div
+        class="rounded-full w-5 h-5"
+        :style="{ background: option.value }"
+      />
+    </template>
+  </USelectMenu>
+</template>
--- src/config/index.ts
+++ src/config/index.ts
@@ -45,3 +45,46 @@ export const themes = [
     value: 'terminal',
   },
 ];
+
+export const backgrounds = [
+  {
+    name: 'linear-gradient(354deg, #ff75b5, #ffb86c)',
+    value: 'linear-gradient(354deg, #ff75b5, #ffb86c)',
+  },
+  {
+    name: 'linear-gradient(140deg, rgb(255, 207, 115), rgb(255, 122, 47))',
+    value: 'linear-gradient(140deg, rgb(255, 207, 115), rgb(255, 122, 47))',
+  },
+  {
+    name: 'linear-gradient(90deg, #93f9b9, #1d976c)',
+    value: 'linear-gradient(90deg, #93f9b9, #1d976c)',
+  },
+  {
+    name: 'linear-gradient(140deg, rgb(142, 199, 251), rgb(28, 85, 170))',
+    value: 'linear-gradient(140deg, rgb(142, 199, 251), rgb(28, 85, 170))',
+  },
+  {
+    name: 'linear-gradient(337deg, #654ea3, #da98b4)',
+    value: 'linear-gradient(337deg, #654ea3, #da98b4)',
+  },
+  {
+    name: '#000',
+    value: '#000',
+  },
+  // {
+  //   name: '#fff',
+  //   value: '#fff',
+  // },
+  {
+    name: 'linear-gradient(270deg, #fc6767, #ec008c)',
+    value: 'linear-gradient(270deg, #fc6767, #ec008c)',
+  },
+  {
+    name: 'linear-gradient(140deg, rgb(165, 142, 251), rgb(233, 191, 248))',
+    value: 'linear-gradient(140deg, rgb(165, 142, 251), rgb(233, 191, 248))',
+  },
+  {
+    name: 'linear-gradient(270deg, #514a9d, #24c6dc)',
+    value: 'linear-gradient(270deg, #514a9d, #24c6dc)',
+  },
+]
