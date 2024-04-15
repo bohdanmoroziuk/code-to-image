27230584b5d927e43885b79cfc6e30fb86e9d062
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2024-04-15 18:17:34 +0300

    feat: export a code snippet to an image

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import htmlToCanvas from 'html2canvas'
+
 import { languages, themes, backgrounds, paddings, getInitialContent } from '~/config'
 
 const language = useState('language', () => nth(languages, -1)!)
@@ -10,11 +12,26 @@ const background = useState('background', () => head(backgrounds)!)
 const padding = useState('padding', () => nth(paddings, 1)!)
 
 const content = useState('content', getInitialContent)
+
+const editor = ref<HTMLDivElement>()
+
+const exportPng = async () => {
+  if (!editor.value) return
+
+  const canvas = await htmlToCanvas(editor.value)
+  const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+
+  const link = document.createElement('a')
+
+  link.download = 'code.png'
+  link.href = image
+  link.click()
+}
 </script>
 
 <template>
   <main class="w-full">
-    <div class="flex items-end gap-x-4 mt-4">
+    <div class="flex items-end gap-x-4 mt-10">
       <UFormGroup
         label="Language"
         class="max-w-60 w-full"
@@ -51,8 +68,18 @@ const content = useState('content', getInitialContent)
           :options="paddings"
         />
       </UFormGroup>
+      <UButton
+        class="whitespace-nowrap"
+        icon="i-heroicons-camera-solid"
+        @click="exportPng"
+      >
+        Export PNG
+      </UButton>
     </div>
-    <div class="mt-10">
+    <div
+      class="mt-10"
+      ref="editor"
+    >
       <CodeEditor
         v-model="content"
         :language="language.value"
